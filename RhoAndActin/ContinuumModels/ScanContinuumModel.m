8771e8bd1a253ba2bd701875ff4fb6565ccf07a7
Ondrej Maxian <ondrejmaxian@Ondrejs-MacBook-Pro.local>
2025-11-29 19:13:09 -0600

    Reorg some files

--- RhoAndActin/ContinuumModels/ScanContinuumModel.m
+++ RhoAndActin/ContinuumModels/ScanContinuumModel.m
@@ -1,6 +1,6 @@
 % koff0, rf, Nuc0, NucEn, koffAct, Arandom, Dv
 % Generate bunch of samples in 2D
-function []=GenerateData(RandomSeed)
+function []=ScanContinuumModel(RandomSeed)
 addpath(genpath('..'))
 try
     RandomSeed=str2num(RandomSeed);
@@ -10,16 +10,17 @@ rng(RandomSeed);
 nSamp = 250;
 nP=10;
 Params = zeros(nSamp,nP);
-PBounds = [0.01 0.5; 0 0.5; 0.5 10];
+PBounds = [0.45 0.75; 0.01 0.5; 0 0.5; 0.5 7];
 for iSamp=1:nSamp
     iSamp
-    p=PBounds(:,1)+(PBounds(:,2)-PBounds(:,1)).*rand(3,1);
-    Nreg1D = round(20/p(3));
+    p=PBounds(:,1)+(PBounds(:,2)-PBounds(:,1)).*rand(size(PBounds,1),1);
+    Nreg1D = round(20/p(4));
     Areg = 400/Nreg1D^2;
-    Params(iSamp,:)=[0.68 0.35 0.3*p(1) 4*p(1) 1*p(1) p(2) 0.05 1 0.1 Areg];
-    Stats=RhoAndActinPDEMod(Params(iSamp,:),0.1,1,1,(RandomSeed-1)*nSamp+iSamp,0);
+    Params(iSamp,:)=[p(1) 0.35 0.3*p(2) 4*p(2) p(2) p(3) 0.05 1 0.1 Areg];
+    Stats=RhoAndActinDiffusionPDEs(Params(iSamp,:),0.1,1,1,...
+        (RandomSeed-1)*nSamp+iSamp,0);
     AllStats{iSamp}=Stats;
     rng("shuffle")
 end
-save(strcat('Scan3P_',num2str(RandomSeed),'.mat'),'Params','AllStats');
+save(strcat('Scan4P_',num2str(RandomSeed),'.mat'),'Params','AllStats');
 end
