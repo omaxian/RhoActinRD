b0051f981c3fdaab518184ed45dc209872ffdb18
Ondrej Maxian <ondrejmaxian@Ondrejs-MacBook-Pro.local>
2026-06-16 14:28:02 -0400

    Rescan over more seeds

--- RhoAndActin/RunSysScan.m
+++ RhoAndActin/RunSysScan.m
@@ -1,13 +1,11 @@
 % For parameter scanning over the forward model
-function []=RunSysScan(RandomSeed)
+function []=RunSysScan(RandomSeed,nPar)
 % Bounds for params
 addpath(genpath('.'))
-nSamp = 500;
+nSamp = 5;
 nSeed = 10;
 nParams = 8;
-numNonZero = 1;
 AllParameters=zeros(nParams,nSamp);
-AllnNzs = zeros(nSamp,1);
 rng(RandomSeed);
 for iSamp=1:nSamp
     iSamp
@@ -15,50 +13,24 @@ for iSamp=1:nSamp
         rng("shuffle")
     end
     xr=rand(5,1);
-    Params = [0.7; 0.1+xr(1)*0.6; 1+xr(2)*39; 1; 1; ...
-        0.1+xr(3)*9.9; 2; xr(4)*100];
-    MonomerClock = Params(6)/Params(4)+Params(6)/Params(5)+Params(3);
-    NucRate_B = Params(7)/(MonomerClock*Params(6));
-    NucRate_Rho = Params(8)/(MonomerClock*Params(6));
-    Params(7)=NucRate_B;
-    Params(8)=NucRate_Rho;
+    if (nPar==5)
+        Params = [0.7; 0.1+xr(1)*0.6; 1+xr(2)*39; 1; 1; ...
+            0.1+xr(3)*9.9; 2; xr(4)*100];
+        MonomerClock = Params(6)/Params(4)+Params(6)/Params(5)+Params(3);
+        NucRate_B = Params(7)/(MonomerClock*Params(6));
+        NucRate_Rho = Params(8)/(MonomerClock*Params(6));
+        Params(7)=NucRate_B;
+        Params(8)=NucRate_Rho;
+    elseif (nPar==2)
+        Params = [0.7; 0.4; 1+xr(2)*39; 1; 1; ...
+            0.1+xr(3)*9.9; 0.054; 0.86];
+    end
     AllParameters(:,iSamp)=Params;
     nNzs=zeros(nSeed,1);
     for seed=1:nSeed
         AllStats{seed}=RhoAndActinBasalNuc(Params,seed,0);
-        if (AllStats{seed}.XCor(1)~=0)
-            nNzs(seed)=1;
-        else
-            nNzs(seed)=0;
-        end
-        if (sum(nNzs)==numNonZero)
-            break;
-        end
     end
-    GoodInds = find(nNzs==1);
-    nNzTot = length(GoodInds);
-    AllnNzs(iSamp)=nNzTot;
-    AvgStats = [];
-    if (nNzTot>0)
-        AllStats=AllStats(GoodInds);
-        AvgStats=AllStats{1};
-        for iP=2:nNzTot
-            AvgStats.MeanRhoHat = AvgStats.MeanRhoHat+AllStats{iP}.MeanRhoHat;
-            AvgStats.MeanActinHat = AvgStats.MeanActinHat+AllStats{iP}.MeanActinHat;
-            AvgStats.ACorsRho = AvgStats.ACorsRho+AllStats{iP}.ACorsRho;
-            AvgStats.ACorsAct = AvgStats.ACorsAct+AllStats{iP}.ACorsAct;
-            AvgStats.XCor = AvgStats.XCor+AllStats{iP}.XCor;
-            AvgStats.MeanActin = AvgStats.MeanActin+AllStats{iP}.MeanActin;
-            AvgStats.ExSizes = [AvgStats.ExSizes; AllStats{iP}.ExSizes];
-        end
-        AvgStats.MeanRhoHat = AvgStats.MeanRhoHat/nNzTot;
-        AvgStats.MeanActinHat = AvgStats.MeanActinHat/nNzTot;
-        AvgStats.ACorsRho = AvgStats.ACorsRho/nNzTot;
-        AvgStats.ACorsAct = AvgStats.ACorsAct/nNzTot;
-        AvgStats.XCor = AvgStats.XCor/nNzTot;
-        AvgStats.MeanActin = AvgStats.MeanActin/nNzTot;
-    end
-    AllAveragedStats{iSamp}=AvgStats;
+    AllStatsAllSeeds{iSamp}=AllStats;
 end
-save(strcat('ScanRGA_',num2str(RandomSeed),'.mat'),'AllAveragedStats',...
-    'AllParameters','AllnNzs')
+save(strcat('Scan',num2str(nPar),'_',num2str(RandomSeed),'.mat'),'AllStatsAllSeeds',...
+    'AllParameters')
